749d2021cd46eae396888096e7dc12565cdfd6ab
Coder731 <stephenobrien1@gmail.com>
2022-03-15 14:03:46 +0000

    Use addClass to add classes to any element needed

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
@@ -1,3 +1,3 @@
 $(document).ready(function() { // anything inside the curly brackets will only run once the DOM is fully loaded. 
-    $("h2").css("text-decoration", "underline")// underlines all <h2> elements
+    $("h2").addClass("underline");// underlines all <h2> elements
 });
