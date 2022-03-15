7561f52d63289dbb948daf3b704fb0d248193a7e
Coder731 <stephenobrien1@gmail.com>
2022-03-15 14:04:35 +0000

    Show use of removeClass function

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
@@ -1,3 +1,3 @@
 $(document).ready(function() { // anything inside the curly brackets will only run once the DOM is fully loaded. 
-    $("h2").addClass("underline");// underlines all <h2> elements
+    $("h2").removeClass("underline");// underlines all <h2> elements
 });
